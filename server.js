f20d9175fdb95d3a8cd1666c6c8f6f44b22e8441
Prosenjit Sardar <prosenjit19111995@gmail.com>
2021-08-06 18:25:42 +0530

    dist name fix

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/covid19-helpline-frontend-MEANstack'));
+app.use(express.static('./dist/covid19-helpline-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/covid19-helpline-frontend-MEANstack/' }),
+    res.sendFile('index.html', { root: 'dist/covid19-helpline-frontend/' }),
 );
 
 // Start the app by listening on the default Heroku port
